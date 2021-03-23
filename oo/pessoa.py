7c426accd0eee71485af425c1bdeb35898886d47
Jeferson Soaris <g7harmonic@outlook.com>
2021-03-22 23:27:09 -0300

    # Inserido Métodos de Classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,14 +2,14 @@
 Aqui será abordado a Orientação a Objeto: Classe, Métodos, Atributos.
 
 Classe:
-*É um molde de instruções contendo um motivo central com sub-propriedades e métodos para chamadas de aplicação;
-*É a base da orientação a objetos;
-*Define a forma como objetos python se comportam;
-*Cada propriedade e método interno podem serem chamados individualmente;
-*É definido com iniciais maiúsculas e CamelCase;
-*Precisa ser iniciada no método (__init__).
-*Precisa ser instanciada para aplicação;
-*Ela deve ser dinâmica.
+* É um molde de instruções contendo um motivo central com sub-propriedades e métodos para chamadas de aplicação;
+* É a base da orientação a objetos;
+* Define a forma como objetos python se comportam;
+* Cada propriedade e método interno podem serem chamados individualmente;
+* É definido com iniciais maiúsculas e CamelCase;
+* Precisa ser iniciada no método (__init__).
+* Precisa ser instanciada para aplicação;
+* Ela deve ser dinâmica.
 
 Mais sobre classe:
 https://www.youtube.com/embed/cV_Soy7jGns
@@ -17,20 +17,27 @@ https://www.youtube.com/embed/gtgI7PC_TJk
 https://www.youtube.com/watch?v=j6B8shHXzks
 https://pt.wikipedia.org/wiki/Classe_(programa%C3%A7%C3%A3o)
 
-Atributo - Métodos:
-*Métodos são funções construidas e pertencentes a uma classe.
-*Cada função ou método contém suas próprias funcionalidade.
-*Os métodos devem conter a palavra chave "Self" como primeiro parâmetro, sendo isolado ou não.
-*Instanciamentos e objetos, se definem apenas ao método iniciado (__init__).
-*Métodos também são atributos.
-
+Atributos:
+1.Métodos de instância def(self).
+* Métodos de instância são funções com atributos de dados que definem a classe (objeto).
+* Os métodos devem conter a palavra chave "Self" como primeiro parâmetro, sendo isolado ou não.
+* O método principal deve ser iniciado com o atributo (__init__).
+* Métodos também são considerados atributos.
 
 Artigo sobre o uso do Self: http://archive.is/cX2mq
 
-Atributo de Dados:
-*São instânciamentos individuais de parâmetros nos métodos;
-*Podem receberem valores e serem chamados individualmente;
-*São considerados objetos.
+2.Atributos de Dados (self.)
+* São instâncias (objetos) conectados aos parâmetros dos métodos;
+* Cada atributo de dado é uma instância (objeto)
+* Permitem serem utilizados individualmente;
+
+3-Método e atributos de Classe x Métodos e atributos de instância:
+
+Método e atributos de Classe - Métodos e atributos diretamente a classe, fora do escopo
+dos métodos de instâncias 'self'.
+
+Métodos e atributos de instância - Métodos e atributos diretamente aos métodos de instâncias
+do objeto, com as palavras chave 'self'.
 '''
 
 
@@ -38,25 +45,41 @@ Atributo de Dados:
 # Usar Camel Case em caso de duas palavras.
 
 
-# Construindo uma Classe (Objeto):
+# Construindo uma Classe (Objeto) que define uma Pessoa:
 class Pessoa:
+    # Local apenas para atributos de dados da classe.
+    # São atributos globais para uso;
+    # Economizam memória;
+    # Podem ser tipos, containers, dicionários;
     olhos = 2
 
-    # Criando um método iniciando a classe:
+    # Métodos de instância do objeto Pessoa.
+    # Inicialização a Classe Pessoa com seus atributos:
     def __init__(self, *filhos, nome="None", idade=40):
         self.idade = idade  # Atributo de dado
         self.nome = nome    # Atributo de dado
         self.filhos = list(filhos) # Atributo de dado com lista.
 
-    # Construindo um Método
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    # Métodos atreladas apenas a Classe. Não dependentes dos métodos de instância:
+    # Estático - Não necessita parâmetros.
+    @staticmethod
+    def metodo_estatico():
+        return "Método Estático"
 
-# Aplicando orientação a objeto:
+    # Método para manipular apenas os atributos de dados direto da Classe:
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+# Manipulando os atributos de dados dos métodos de instância da Classe:
 if __name__ == '__main__':
-    jeferson = Pessoa(nome='Jeferson') # Atribuindo o objeto, passando um dado de parâmetro. Aqui ele é iniciado.
+    # Instanciando um novo objeto atribuindo a classe e iniciando para manipulação:
+    jeferson = Pessoa(nome='Jeferson')
     print(jeferson.nome)
+
     pingo = Pessoa(jeferson, nome='Pingo')
     print(id(pingo))
     print(pingo.cumprimentar()) # Modo correto.
@@ -76,7 +99,7 @@ if __name__ == '__main__':
     print(jeferson.__dict__)
     print(pingo.__dict__)
 
-    # Aplicando atributos de classe:
+    # Manipulando apenas os atributos globais da Classe:
     print(f'Toda pessoa tem {Pessoa.olhos} olhos')
     print(f'O meu cachorro {pingo.nome}, tem {pingo.olhos} olhos.')
     print(f'O {jeferson.nome}, tem {jeferson.olhos} olhos.')
@@ -89,3 +112,10 @@ if __name__ == '__main__':
     print(pingo.__dict__)
     # Atributo da classe com novo ID a cada modificação:
     print(id(jeferson.olhos), id(pingo.olhos))
+
+    # Manipulando métodos de classe.
+    # @staticmethod - Manipula dados livremente, não necessita de parâmetros.
+    print(Pessoa.metodo_estatico(), pingo.metodo_estatico())
+
+    # @classmethod - Manipula atributos da classe:
+    print(Pessoa.nome_e_atributos_de_classe(), pingo.nome_e_atributos_de_classe())
